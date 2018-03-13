193f50ed39760a357d1f7f1481d8c133273c4822
Hemant Joshi <codingdefined@gmail.com>
2018-03-13 16:51:30 +0530

    Adding Memo Check

--- contentscript.js
+++ contentscript.js
@@ -15,4 +15,15 @@
         var str = "You have loaded " + window.location.hostname + " site which seems to be a phising link, correct link is https://steemit.com";
         alert(str);
     }
+    if (url.includes("steemit.com") && url.includes("transfers")) {
+        var allMemoSpans = document.getElementsByClassName('Memo');
+        var arrayNumbers = [];
+
+        for (var i = 0; i < allMemoSpans.length; i++) {
+            var previousMemo = allMemoSpans[i].innerHTML;
+            if ((allMemoSpans[i].innerHTML.includes("http") || allMemoSpans[i].innerHTML.includes("https")) && !allMemoSpans[i].innerHTML.includes("steemit.com")) {
+                allMemoSpans[i].innerHTML = "<div style='color:red;'>The link in the memo is not Steemit Link</div> <br>" + allMemoSpans[i].innerHTML;
+            }
+        }
+    }
 })();
