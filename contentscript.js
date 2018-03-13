fcfebd4f8c34c9d189598b4b15d5a43ecb93703f
Hemant Joshi <codingdefined@gmail.com>
2018-03-13 16:40:51 +0530

    Create contentscript.js

---
+++ contentscript.js
@@ -0,0 +1,14 @@
+(function () {
+    var url = location.href.toLowerCase();
+    var password = document.getElementsByName("password");
+    if (password.length > 0) {
+        var placeHolderText = password.getAttribute("placeholder");
+        if (password && placeHolderText === "Password or WIF" &&
+                !(url.includes("steemit.com") ||
+                url.includes("steemd.com") ||
+                url.includes("busy.org") ||
+                url.includes("utopian.io"))) {
+            var str = "You have loaded " + window.location.hostname + " site which seems to be a phising link, correct link is https://steemit.com";
+        }
+    }
+})();
