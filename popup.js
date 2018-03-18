1fd31978077f6946c000d82d7e15f9f66bbc98ed
Amos Bastian <amos_bastian@hotmail.com>
2018-03-18 14:41:25 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,6 +2,6 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
   if(document.getElementById('currentsite') !== null) {
     var url = new URL(tabs[0].url)
     var domain = url.hostname
-    document.getElementById('currentsite').innerHTML = "You are on <b>" + domain + "</b>, if you intended to go to Steemit.com, the correct link is <b><a href='https://steemit.com'>Steemit.com</a></a>";
+    document.getElementById('currentsite').innerHTML = "You are currently on <b>" + domain + "</b>, if you intended to go to Steemit, then the correct link is <b><a href='https://steemit.com'>Steemit.com</a></b>.";
   }
 });
